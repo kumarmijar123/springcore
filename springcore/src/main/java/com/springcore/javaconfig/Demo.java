90ec53b62088369b3e1dd3ca8aebc1ac877a8f65
Admin <mijarkumar2019@gmail.com>
2023-07-08 22:19:20 +0545

    configuration

---
+++ springcore/src/main/java/com/springcore/javaconfig/Demo.java
@@ -0,0 +1,21 @@
+package com.springcore.javaconfig;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class Demo {
+
+	public static void main(String[] args) {
+		ApplicationContext context=
+				new AnnotationConfigApplicationContext(JavaConfig.class);
+
+		
+	   Student student = context.getBean("getStudent", Student.class);
+	   System.out.println(student);
+	   student.study();
+	   
+	   
+	}
+
+}
